2c5fc1cad2ecc31638ebbfaa3cd3ceaa534a16c4
Tim <sterzeltim@gmail.com>
2016-11-29 18:18:56 +0100

    Hide table at start and add file dialog

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,24 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QFileDialog>
+#include <QString>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->tableWidget_data->setColumnCount(6);
+    ui->tableWidget_data->setRowCount(3);
+    ui->tableWidget_data->hide();
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_action_ffnen_triggered()
+{
+    QString fileName = QFileDialog::getOpenFileName(this, tr("Öffnen"));
 }
