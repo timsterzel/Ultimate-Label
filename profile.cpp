3332443fbe0899b36d934514f8765f7c607637b7
TmCrafz <info@tmcrafz.org>
2016-12-02 15:00:28 +0100

    Implement Json read and write metjhods to Profile class

--- profile.cpp
+++ profile.cpp
@@ -4,3 +4,56 @@ Profile::Profile()
 {
 
 }
+
+Profile::Profile(QString name, QString seperator, QString templateText)
+: m_name{ name }
+, m_seperator{ seperator }
+, m_templateText{ templateText }
+{
+
+}
+
+QString Profile::getName() const
+{
+    return m_name;
+}
+
+void Profile::setName(QString name)
+{
+    m_name = name;
+}
+
+QString Profile::getSeperator() const
+{
+    return m_seperator;
+}
+
+void Profile::setSeperator(QString seperator)
+{
+    m_seperator = seperator;
+}
+
+QString Profile::getTemplateText() const
+{
+    return m_templateText;
+}
+
+void Profile::setTemplateText(QString templateText)
+{
+    m_templateText = templateText;
+}
+
+void Profile::writeToJson(QJsonObject &json)
+{
+    json[JSON_NAME] = m_name;
+    json[JSON_SEPERATOR] = m_seperator;
+    json[JSON_TEMPLATETEXT] = m_templateText;
+}
+
+void Profile::readFromJson(QJsonObject &json) const
+{
+    m_name = json[JSON_NAME].toString();
+    m_seperator = json[JSON_SEPERATOR].toString();
+    m_templateText = json[JSON_TEMPLATETEXT].toString();
+
+}
--- profile.h
+++ profile.h
@@ -1,11 +1,34 @@
 #ifndef PROFILE_H
 #define PROFILE_H
-
+#include <QString>
+#include <QJsonObject>
 
 class Profile
 {
+private:
+    // Strings which are used for reading and writing Json files
+    static const QString JSON_NAME = "name";
+    static const QString JSON_SEPERATOR = "seperator";
+    static const QString JSON_TEMPLATETEXT = "templateText";
+
+    QString m_name;
+    QString m_seperator;
+    QString m_templateText;
 public:
     Profile();
-};
+    Profile(QString name, QString seperator, QString templateText);
+
+    QString getName() const;
+    void setName(QString name);
+
+    QString getSeperator() const;
+    void setSeperator(QString seperator);
+
+    QString getTemplateText() const;
+    void setTemplateText(QString templateText);
+
+    void writeToJson(QJsonObject &json);
+    void readFromJson(QJsonObject &json) const;
+}
 
 #endif // PROFILE_H
