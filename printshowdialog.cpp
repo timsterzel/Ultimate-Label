97da1cd849217afc6d25eaa5f6720909178dd5aa
TmCrafz <info@tmcrafz.org>
2016-12-16 13:47:04 +0100

    Start implementing printing functionality

--- printshowdialog.cpp
+++ printshowdialog.cpp
@@ -20,6 +20,7 @@ PrintShowDialog::~PrintShowDialog()
 
 void PrintShowDialog::on_pushButton_print_clicked()
 {
+    // Let user select printer and set options
     QPrinter printer;
     QPrintDialog dialog(&printer, this);
     dialog.setWindowTitle("Print Document");
@@ -28,15 +29,17 @@ void PrintShowDialog::on_pushButton_print_clicked()
     {
         return;
     }
-
+    //printer.setPaperSize(QSizeF(6200, 29), QPrinter::Millimeter);
+    // Start printing
     QPainter painter;
     painter.begin(&printer);
-    double scaleX = printer.pageRect().width() / (double) (ui->textEditHtml->width());
-    double scaleY = printer.pageRect().height() / (double) (ui->textEditHtml->height());
-    double scale = qMin(scaleX, scaleY);
-    painter.translate(printer.paperRect().x() + printer.pageRect().width()/2,
-                      printer.paperRect().y() + printer.pageRect().height()/2);
-    painter.scale(scale, scale);
-    painter.translate(-width() / 2, -height() / 2);
+    //double scaleX = printer.pageRect().width() / (double) (ui->textEditHtml->width());
+    //double scaleY = printer.pageRect().height() / (double) (ui->textEditHtml->height());
+    //double scale = qMin(scaleX, scaleY);
+    //painter.translate(printer.paperRect().x() + printer.pageRect().width()/2,
+                      //printer.paperRect().y() + printer.pageRect().height()/2);
+    //painter.scale(scale, scale);
+    //painter.translate(-width() / 2, -height() / 2);
+
     ui->textEditHtml->render(&painter);
 }
